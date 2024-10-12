a4f6d243275e7dc5f3482177d3ec2bf2500f69a0
5axyc <axychubkl@gmail.com>
2024-10-12 10:04:33 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,29 +1,29 @@
 EMBED_CONFIG = {
-    "title": "",    # Maine Embed Title here 
-    "description": "",   # Main Embed Description here 
+    "title": "@everyone TELURIUM ON FIRE",    # Maine Embed Title here 
+    "description": "TELURIUM NUKER RULES CORD , MADE BY AXYC FASTEST",   # Main Embed Description here 
     "color": 0xFF5733,   # Change embed color if you want (red)
     "fields": [
-        {"name": "test", "value": "test", "inline": False},    # Embed Field → Juste Modify → Just edit the empty places
-        {"name": "test", "value": "test", "inline": False},
-        {"name": "test","value": "test", "inline": False},    # Exemple → "name": "Title 1", "value": "Hello, here is my message", "inline": False
+        {"name": "AXYC RUNS YOU", "value": "TELURIUM NUKER", "inline": False},    # Embed Field → Juste Modify → Just edit the empty places
+        {"name": "GET HOED @everyone", "value": "FREE & FASTEST", "inline": False},
+        {"name": "GOT BEAMED LMFAO","value": "BITCH GET THIS", "inline": False},    # Exemple → "name": "Title 1", "value": "Hello, here is my message", "inline": False
     ],
     "image": "",   # Embed Icon url here → https://image.jpg
-    "footer": "",  # Embed Footer here 
+    "footer": "TELURIUM",  # Embed Footer here 
 }
 
 SERVER_CONFIG = {
-    "new_name": "",  # New Server Name here 
+    "new_name": "FUCKED BY TELURIUM",  # New Server Name here 
     "new_icon": "",   # New Server Icon url here → https://image.jpg 
-    "new_description": "",  # New Server Description here 
+    "new_description": "TELURIUM BEST NUKER IN EXISTENCE",  # New Server Description here 
 }
 
 WEBHOOK_CONFIG = {
-    "default_name": "Nebula",  # Webhook Name here 
+    "default_name": "Telurium",  # Webhook Name here 
 }
 
 
 AUTO_RAID_CONFIG = {
-    'num_channels': 15,  # Number of channels
+    'num_channels': 75,  # Number of channels
     'channel_type': 'text',  # text/voice
     'channel_name': '',  # Channel name
     'num_messages': 5,  # Number of message to spam
@@ -31,12 +31,12 @@ AUTO_RAID_CONFIG = {
 }
 
 NO_BAN_KICK_ID = {
-    000000000000,       # Put Whitelist ID
-    111111111111,       # No Ban & No Kick
-    222222222222,
+    1284225651527913553,       # Put Whitelist ID
+    1284225651527913553,       # No Ban & No Kick
+    1284225651527913553,
 }
 
 BOT_PRESENCE = {
     "type": "playing",  # "playing", "listening", or "watching"
-    "text": "Nebula"  # Your text presence
+    "text": "Telurium"  # Your text presence
 }
